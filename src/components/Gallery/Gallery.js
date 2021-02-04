be20b7b26c33d2e59c02c287d4962cdca961fda6
mike <mikechan7451@gmail.com>
2021-02-04 22:59:14 +0800

    fix image location

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,67 +1,83 @@
 import React from 'react';
-import './Gallery.css';
+import './Gallery.css'
+import img1 from '../../images/1.jpg'
+import img2 from '../../images/2.jpg'
+import img3 from '../../images/3.jpg'
+import img4 from '../../images/4.png'
+import img5 from '../../images/5.png'
+import img6 from '../../images/6.png'
+import img7 from '../../images/7.jpg'
+import img8 from '../../images/8.png'
+import img9 from '../../images/9.jpg'
+import img10 from '../../images/10.png'
+import img11 from '../../images/11.png'
+import img12 from '../../images/12.png'
+import img13 from '../../images/13.jpg'
+import img14 from '../../images/14.jpg'
+import img15 from '../../images/15.jpg'
+
 
 function Gallery() {
     return (
         <div className="container4">
             <div className="box2">
-                <img src="images/1.jpg" />
+                <img alt="11" src={img1} />
                 {/* <span>CSS</span> */}
             </div>
             <div className="box2">
-                <img src="images/2.jpg" />
+                <img alt="12" src={img2} />
                 {/* <span>Image</span> */}
             </div>
             <div className="box2">
-                <img src="images/2021-02-04 174740.jpg" />
+                <img alt="13" src={img3} />
                 {/* <span>Hover</span> */}
             </div>
             <div className="box2">
-                <img src="images/12.4.png" />
+                <img alt="14" src={img4} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/12.7.png" />
+                <img alt="15" src={img5} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/11.6.1.png" />
+                <img alt="16" src={img6} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/2021-02-04 174904.jpg" />
+                <img alt="17" src={img7} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/21.0.png" />
+                <img alt="18" src={img8} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/2021-02-04 175053.jpg" />
+                <img alt="19" src={img9} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/13.0.png" />
+                <img alt="111" src={img10} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/15.1.png" />
+                <img alt="122" src={img11} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/16.2.png" />
+                <img alt="133" src={img12} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/2021-02-04 175203.jpg" />
+                <img alt="144" src={img13} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/11.4.jpg" />
+                <img alt="155" src={img14} />
                 {/* <span>Effect</span> */}
             </div>
             <div className="box2">
-                <img src="images/2021-02-04 175244.jpg" />
+                <img alt="144" src={img15} />
                 {/* <span>Effect</span> */}
             </div>
         </div>
--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../../App.css';
-import Background from '../Background/background'
+// import Background from '../Background/background'
 import { motion } from 'framer-motion';
 import { animationOne } from '../animations'
 import Footer from '../Footer';
-import Pure_Background from '../Pure_Background/pure_background';
-import Cards from '../Cards'
+// import Pure_Background from '../Pure_Background/pure_background';
+// import Cards from '../Cards'
 import Gallery from '../Gallery/Gallery';
 
 export default function Work() {
